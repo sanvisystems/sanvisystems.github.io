85761a27ab9b4c7e8a8ce210bc959c2b4066d5a4
sanvisystems <38746041+sanvisystems@users.noreply.github.com>
2018-12-13 23:33:51 +0600

    Add files via upload

---
+++ precache-manifest.2cc37de82c7831270cb99c7abb506aa5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a311dbb31ec4d4120337",
+    "url": "/static/js/main.a311dbb3.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "204e7dfd27778f815a5d942d838cae31",
+    "url": "/index.html"
+  }
+];
